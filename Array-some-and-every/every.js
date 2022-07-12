9a077e6c5cea3bc992afa8d8c7386bca61a1c57f
Yuhan Zhang <zyh514312@gmail.com>
2022-07-12 02:57:24 +0000

    correction

--- Array-some-and-every/every.js
+++ Array-some-and-every/every.js
@@ -1,12 +1,10 @@
 /* eslint-disable no-console */
 function every(array, predicate) {
-  var count = 0;
   for (var i = 0; i < array.length; i++) {
-    if (predicate(array[i])) count++;
+    if (!predicate(array[i])) return false;
   }
-  if (count === array.length) return true;
-  else return false;
+  return true;
 }
-const array = [2, 4, 5, 6];
+const array = [2, 4, 8, 6, 10];
 const even = element => element % 2 === 0;
 console.log(every(array, even));
