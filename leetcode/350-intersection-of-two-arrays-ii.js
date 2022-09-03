042653a5558e609e3de59b2d338b11fc2cee3e5e
Yuhan Zhang <zyh514312@gmail.com>
2022-09-03 15:33:33 +0000

    intersect done

--- leetcode/350-intersection-of-two-arrays-ii.js
+++ leetcode/350-intersection-of-two-arrays-ii.js
@@ -3,5 +3,13 @@ Given two integer arrays nums1 and nums2, return an array of their intersection.
 */
 // eslint-disable-next-line no-unused-vars
 var intersect = function (nums1, nums2) {
-
+  const sol = [];
+  for (let i = 0; i < nums2.length; i++) {
+    const index = nums1.indexOf(nums2[i]);
+    if (index >= 0) {
+      sol.push(nums2[i]);
+      nums1.splice(index, 1);
+    }
+  }
+  return sol.sort();
 };
